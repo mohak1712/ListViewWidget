f48a63430efe78809a01ab8dd1bea27bd8bb8c5a
Mohak <mohakpuri1712@gmail.com>
2017-04-14 13:59:31 +0530

    Widget Service added

---
+++ app/src/main/java/mohak/com/listviewwidget/WidgetService.java
@@ -0,0 +1,13 @@
+package mohak.com.listviewwidget;
+
+
+import android.content.Intent;
+import android.widget.RemoteViewsService;
+
+public class WidgetService extends RemoteViewsService {
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+
+        return new WidgetAdapter(this);
+    }
+}
